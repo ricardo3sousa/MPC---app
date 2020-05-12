71bfe9a8f4b171140e1c9dd521f01ae9e19d7a31
Ricardo Sousa <33496977+ricardo3sousa@users.noreply.github.com>
2020-05-12 20:11:13 +0100

    my commit

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-window.addEventListener('keydown', function(e){
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    if(!audio) return;
+let teclas = document.querySelector('.teclas');
+
+teclas.addEventListener('click', (event) => {
+    const element = event.target;
+    const elementKey = element.attributes.dataKey.value;
+    play(elementKey);
+});
+
+window.addEventListener('keydown', function (event) {
+    let valor = event.keyCode;
+    play(valor);
+});
+
+function play(val) {
+    const audio = document.querySelector(`audio[dataKey="${val}"]`);
+    const key = document.querySelector(`.key[dataKey="${val}"]`);
+    if (!audio) return;
     audio.currentTime = 0;
     audio.play();
     key.classList.add('play')
     setTimeout(() => {
-       key.classList.remove('play'); 
-    },100)
-    
-})
+        key.classList.remove('play');
+    }, 100)
+}
